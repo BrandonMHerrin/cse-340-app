a9c8fc19cf09ed8b43fff93de6747410ba37259a
BrandonMHerrin <brandon.herrin@live.com>
2024-10-12 17:09:45 -0600

    bug: Validation Issues Inventory Page

--- utilities/index.js
+++ utilities/index.js
@@ -43,6 +43,10 @@ Util.buildClassificationGrid = async function(data) {
             grid += '<div class="namePrice">';
             grid += '<hr />';
             grid += '<h2>';
+            grid += '<a href="../../inv/detail/'+ vehicle.inv_id + '" title="View '
+            + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">'
+            + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>';
+            grid += '</h2>'
             grid += '<span>$'
             + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>';
             grid += '</div>';
